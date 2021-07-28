c0bd70196053908a0b5b4c11462a5355ceb55245
sqttrainer <yoursqttrainer@gmail.com>
2021-07-28 07:58:56 +0530

    added full project

---
+++ src/test/java/pack2/LoginPageTest.java
@@ -0,0 +1,22 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+	
+	@Test
+	public void verifyLoginwithCorrectPassword()
+	{
+		
+		
+	}
+	
+	
+	@Test
+	public void verifyLoginwithIncorrectPassword()
+	{
+		
+		
+	}
+
+}
